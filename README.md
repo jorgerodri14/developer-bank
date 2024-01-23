ad54f970f82744c2bd6aa933980ea4e73a4119e1
Jorge Rodríguez Sánchez <jorosadev@gmail.com>
2024-01-23 16:26:43 +0000

    chore: create minimal config

---
+++ README.md
@@ -0,0 +1,12 @@
+# DeveloperBank API
+
+1. Clonar proyectos
+2. `npm install`
+3. Clonar archivo `.env.dev` y renombrarlo a `.env`
+4. Levantar la base de datos
+
+```
+docker-compose up -d
+```
+
+5. Levantar proyecto en desarrollo: `npm run start:dev`
---
+++ src/database/database.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true, //! Configuración de desarrollo
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
